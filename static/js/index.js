86c79a5abeaa817952c30654f310062a9b3e9ba0
Alioune CISSE <60492447+Alioune-Cisse@users.noreply.github.com>
2022-08-23 16:00:34 +0000

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ function ajouter_val(){
 
 var span = document.createElement('SPAN');
     textes = document.getElementById("tags").value
-    span.innerHTML = '<input type="checkbox" id='+textes+'class="chk-btn" name="choix" value='+textes+' checked/><label for="choix">'+textes+'</label><br>'
+    span.innerHTML = '<input type="checkbox" id="'+textes+'" class="chk-btn" name="choix" value="'+textes+'" checked/><label for="choix">"'+textes+'"</label><br>'
     span.id = name;
     span.className = "filelist";
     document.getElementById("tags").value = ""
